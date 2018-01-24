c8fa016229abc3caa35e7d9387e01ed315be4271
Austin Jenchi <ajenchi9@gmail.com>
2018-01-23 22:25:28 -0800

    Use TalonSRX Follower mode instead of WPILib SpeedControllerGroups
    This uses the built in Talon SRX follower mode to copy the other Talons' output. This prepares for more advanced features later, like controlling the left and right sides with PID (i.e. PID to a set velocity or position), and probably also lightens the load just a tiny bit on the RIO (O(N) to O(1) where N is the number of controllers in the group?), as following is handled by the Talons themselves on their hardware instead of the for loop running on the RIO from SpeedControllerGroup.

--- src/main/java/org/team1294/firstpowerup/robot/subsystems/DriveSubsystem.java
+++ src/main/java/org/team1294/firstpowerup/robot/subsystems/DriveSubsystem.java
@@ -1,7 +1,7 @@
 package org.team1294.firstpowerup.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import org.team1294.firstpowerup.robot.RobotMap;
@@ -19,17 +19,16 @@ public class DriveSubsystem extends Subsystem {
 
     public DriveSubsystem() {
         super("Drivetrain Subsystem");
+
         leftFront = new WPI_TalonSRX(RobotMap.TALON_LEFT_FRONT);
         leftRear = new WPI_TalonSRX(RobotMap.TALON_LEFT_REAR);
         rightFront = new WPI_TalonSRX(RobotMap.TALON_RIGHT_FRONT);
         rightRear = new WPI_TalonSRX(RobotMap.TALON_RIGHT_REAR);
 
-        SpeedControllerGroup left = new SpeedControllerGroup(leftFront,
-                leftRear);
-        SpeedControllerGroup right = new SpeedControllerGroup(rightFront,
-                rightRear);
+        leftRear.set(ControlMode.Follower, RobotMap.TALON_LEFT_FRONT);
+        rightRear.set(ControlMode.Follower, RobotMap.TALON_RIGHT_FRONT);
 
-        drive = new DifferentialDrive(left, right);
+        drive = new DifferentialDrive(leftFront, rightFront);
     }
 
     public void arcadeDrive(double forward, double turn) {
