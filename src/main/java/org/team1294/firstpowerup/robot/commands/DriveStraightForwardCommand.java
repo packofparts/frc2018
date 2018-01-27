fdbac6b379a7949ab784ba69b12623b1e7069075
Austin Jenchi <ajenchi9@gmail.com>
2018-01-27 11:49:11 -0800

    Remove SmartDashboard tuning of kP and kI

--- src/main/java/org/team1294/firstpowerup/robot/commands/DriveStraightForwardCommand.java
+++ src/main/java/org/team1294/firstpowerup/robot/commands/DriveStraightForwardCommand.java
@@ -6,8 +6,8 @@ import org.team1294.firstpowerup.robot.Robot;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class DriveStraightForwardCommand extends PIDCommand {
-    private static /* final */ double p = 0.5;
-    private static /* final */ double i = 0.0;
+    private static final double p = 1.0;
+    private static final double i = 0.1;
     private static final double d = 0.0;
     private static final double TOLERANCE = 0.1;
     private static final double MAX_SPEED = 0.5;
@@ -24,16 +24,10 @@ public class DriveStraightForwardCommand extends PIDCommand {
         getPIDController().setAbsoluteTolerance(TOLERANCE);
         getPIDController().setOutputRange(-MAX_SPEED, MAX_SPEED);
         getPIDController().setSetpoint(Robot.driveSubsystem.getEncoderPositionAverage());
-
-        SmartDashboard.putNumber("DSFC/p", 1.0);
-        SmartDashboard.putNumber("DSFC/i", 0.1);
     }
 
     @Override
     protected void initialize() {
-        getPIDController().setP(SmartDashboard.getNumber("DSFC/p", 1.0));
-        getPIDController().setI(SmartDashboard.getNumber("DSFC/i", 0.1));
-
         getPIDController().setSetpoint(Robot.driveSubsystem.getEncoderPositionAverage() + distance);
         CommandGroup group = getGroup();
         if (group instanceof DriveStraightCommand) {
