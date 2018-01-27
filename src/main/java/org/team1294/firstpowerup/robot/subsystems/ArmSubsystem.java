0b2caaadd1f8120a7eb0253ec3964a5829cecb57
Austin Jenchi <ajenchi9@gmail.com>
2018-01-27 14:23:01 -0800

    Add ArmSubsystem

---
+++ src/main/java/org/team1294/firstpowerup/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,39 @@
+package org.team1294.firstpowerup.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.team1294.firstpowerup.robot.RobotMap;
+
+/**
+ * @author Austin Jenchi (timtim17)
+ */
+public class ArmSubsystem extends Subsystem {
+    private TalonSRX motor;
+    private AnalogPotentiometer potentiometer;
+
+    public ArmSubsystem() {
+        super("Arm Subsystem");
+        motor = new TalonSRX(RobotMap.TALON_ARM);
+        potentiometer = new AnalogPotentiometer(RobotMap.SENSOR_POT);
+    }
+
+    public void driveTalon(double output) {
+        motor.set(ControlMode.PercentOutput, output);
+    }
+
+    public double getPotOutput() {
+        return potentiometer.get();
+    }
+
+    @Override
+    public void periodic() {
+        // do nothing
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+//        setDefaultCommand();
+    }
+}
