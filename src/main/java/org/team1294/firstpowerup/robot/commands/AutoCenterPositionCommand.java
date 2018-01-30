bf28ab7a43067fd0c5dfef91fe04ff34e1e7077f
jxlewis <jxlewis@godaddy.com>
2018-01-30 14:09:28 -0800

    added velocity and turnRate arguments

--- src/main/java/org/team1294/firstpowerup/robot/commands/AutoCenterPositionCommand.java
+++ src/main/java/org/team1294/firstpowerup/robot/commands/AutoCenterPositionCommand.java
@@ -8,23 +8,20 @@ public class AutoCenterPositionCommand extends CommandGroup {
     public AutoCenterPositionCommand() {
 
         // drive straight ahead far enough to make it easy to turn
-        addSequential(new AutoDriveCommand(0.1, 0));
+        addSequential(new AutoDriveCommand(0.1, 0, 0.5, 0.25));
 
         // drive towards the desired side
         final String gameData = DriverStation.getInstance().getGameSpecificMessage();
         if (gameData.charAt(0) == 'L') {
-            addSequential(new AutoDriveCommand(0, 315));
-            addSequential(new AutoDriveCommand(1.0, 315)); // todo tune this distance
+            addSequential(new AutoDriveCommand(0, 315, 0.5, 0.5));
+            addSequential(new AutoDriveCommand(1.0, 315, 0.5, 0.25)); // todo tune this distance
         } else {
-            addSequential(new AutoDriveCommand(0, 45));
-            addSequential(new AutoDriveCommand(1.0, 45)); // todo tune this distance
+            addSequential(new AutoDriveCommand(0, 45, 0.5, 0.5));
+            addSequential(new AutoDriveCommand(1.0, 45, 0.5, 0.25)); // todo tune this distance
         }
 
         // turn towards the vision target
-        addSequential(new AutoDriveCommand(0, 0));
-
-        // approach the vision target
-        addSequential(new AutoDriveCommand(0.5, 0));
+        addSequential(new AutoDriveCommand(0, 0, 0.5, 0.5));
 
         // engage the vision system approach
         addSequential(new AutoVisionTargetCommand(1.0));
--- src/main/java/org/team1294/firstpowerup/robot/commands/AutoSidePositionCommand.java
+++ src/main/java/org/team1294/firstpowerup/robot/commands/AutoSidePositionCommand.java
@@ -11,53 +11,53 @@ public class AutoSidePositionCommand extends CommandGroup {
         final String switchSide = gameData.substring(0, 0);
 
         // drive straight ahead far enough to make it easy to turn
-        addSequential(new AutoDriveCommand(0.1, 0)); // todo tune this distance
+        addSequential(new AutoDriveCommand(0.1, 0, 0.5, 0.25)); // todo tune this distance
 
         // drive towards the wall
         if (side.equals("L")) {
-            addSequential(new AutoDriveCommand(0, 315));
-            addSequential(new AutoDriveCommand(0.5, 315)); // todo tune this distance
+            addSequential(new AutoDriveCommand(0, 315, 0.5, 0.5));
+            addSequential(new AutoDriveCommand(0.5, 315, 0.5, 0.25)); // todo tune this distance
         } else {
-            addSequential(new AutoDriveCommand(0, 45));
-            addSequential(new AutoDriveCommand(0.5, 45)); // todo tune this distance
+            addSequential(new AutoDriveCommand(0, 45, 0.5, 0.5));
+            addSequential(new AutoDriveCommand(0.5, 45, 0.5, 0.25)); // todo tune this distance
         }
 
         // turn downfield
-        addSequential(new AutoDriveCommand(0, 0));
+        addSequential(new AutoDriveCommand(0, 0, 0.5, 0.5));
 
         if (scaleSide.equals(side)) {
             // drive downfield until abeam the scale
-            addSequential(new AutoDriveCommand(2.0, 0)); // todo tune this distance
+            addSequential(new AutoDriveCommand(2.0, 0, 0.5, 0.25)); // todo tune this distance
 
             // drive towards the scale
             if (side.equals("L")) {
-                addSequential(new AutoDriveCommand(0, 90));
-                addSequential(new AutoDriveCommand(0.5, 90)); // todo tune this distance
+                addSequential(new AutoDriveCommand(0, 90, 0.5, 0.5));
+                addSequential(new AutoDriveCommand(0.5, 90, 0.5, 0.25)); // todo tune this distance
             } else {
-                addSequential(new AutoDriveCommand(0, 270));
-                addSequential(new AutoDriveCommand(0.5, 270)); // todo tune this distance
+                addSequential(new AutoDriveCommand(0, 270, 0.5, 0.5));
+                addSequential(new AutoDriveCommand(0.5, 270, 0.5, 0.25)); // todo tune this distance
             }
 
             // deliver the crate
             // todo
         } else if (switchSide.equals(side)) {
             // drive downfield until abeam the switch
-            addSequential(new AutoDriveCommand(1.0, 0)); // todo tune this distance
+            addSequential(new AutoDriveCommand(1.0, 0, 0.5, 0.25)); // todo tune this distance
 
             // drive towards the switch
             if (side.equals("L")) {
-                addSequential(new AutoDriveCommand(0, 90));
-                addSequential(new AutoDriveCommand(0.5, 90)); // todo tune this distance
+                addSequential(new AutoDriveCommand(0, 90, 0.5, 0.5));
+                addSequential(new AutoDriveCommand(0.5, 90, 0.5, 0.25)); // todo tune this distance
             } else {
-                addSequential(new AutoDriveCommand(0, 270));
-                addSequential(new AutoDriveCommand(0.5, 270)); // todo tune this distance
+                addSequential(new AutoDriveCommand(0, 270, 0.5, 0.5));
+                addSequential(new AutoDriveCommand(0.5, 270, 0.5, 0.25)); // todo tune this distance
             }
 
             // deliver the crate
             // todo
         } else {
             // drive downfield until past the auto line
-            addSequential(new AutoDriveCommand(1.0, 0)); // todo tune this distance
+            addSequential(new AutoDriveCommand(1.0, 0, 0.5, 0.25)); // todo tune this distance
         }
 
 
