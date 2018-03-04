e883a30fb1a91ad3a7abad7facca8898bf6e478f
Austin Jenchi <ajenchi9@gmail.com>
2018-03-03 21:18:27 -0800

    add check for side and change angle in simple deliver cube

--- src/main/java/org/team1294/firstpowerup/robot/commands/AutoShortSideDeliverSimple.java
+++ src/main/java/org/team1294/firstpowerup/robot/commands/AutoShortSideDeliverSimple.java
@@ -12,8 +12,9 @@ public class AutoShortSideDeliverSimple extends CommandGroup {
     public AutoShortSideDeliverSimple(char side) {
         super("SBBL deliver cube to " + side);
         this.side = side;
+        int heading = (side == 'L') ? 90 : 270;
         addSequential(new AutoDriveCommand(1.4));
-        addSequential(new AutoDriveCommand(0, 90, 0.5, 0.75));
+        addSequential(new AutoDriveCommand(0, heading, 0.5, 0.75));
         addSequential(new SetArmHeightCommand(500));
         addSequential(new AutoDeliverCrateCommand());
     }
